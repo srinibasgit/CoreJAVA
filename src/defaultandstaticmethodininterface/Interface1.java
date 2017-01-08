676eec31f48a9c817e510c897254f3d9c2701e05
Srinibas <srinibas.bam@gmail.com>
2017-01-08 18:50:16 +0530

    Added some stream examples

--- src/defaultandstaticmethodininterface/Interface1.java
+++ src/defaultandstaticmethodininterface/Interface1.java
@@ -1,5 +1,14 @@
 package defaultandstaticmethodininterface;
 
+/**
+ * An interface with exactly one abstract method becomes Functional Interface.
+ * We don’t need to use @FunctionalInterface annotation to mark an interface as
+ * Functional Interface. @FunctionalInterface annotation is a facility to avoid
+ * accidental addition of abstract methods in the functional interfaces.
+ * 
+ * @author spanda
+ *
+ */
 @FunctionalInterface
 public interface Interface1 {
 	void method1(String str);
